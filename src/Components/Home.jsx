0321ca36d57501c033d689b48fb9680322b37b07
Amol Talekar <amol11talekar@gmail.com>
2021-07-28 00:14:32 +0530

    animation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,6 +8,11 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 import SystemUpdateAltIcon from '@material-ui/icons/SystemUpdateAlt';
+import { Wave } from 'react-animated-text';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 
 const useStyles = makeStyles({
@@ -56,6 +61,8 @@ const Home = () => {
         link.href = "AmolTalekarResume.pdf";
         link.click();
       };
+
+   
     return (
         <Box boxShadow={3} >
            
@@ -66,8 +73,8 @@ const Home = () => {
                 </div>
                
                <div>
-                <h1 className={styles.abouth2Class}>Hi there, I am <span style={{fontWeight:"bolder",fontStyle:"italic"}}>Amol Talekar</span> , it is very nice to meet you!</h1>
-                <h2 className={styles.abouth2Class}>I am a Passionate aspiring Full Stack Developer skilled in MERN stack, moulded and shaped by Masai school’s intensive and immersive learning.  Look forward to joining a company where I will  be able to contribute towards individual and company growth.</h2>
+                <h1 className={styles.abouth2Class}><span  ><Wave  text="Hi there, I am  Amol Talekar," ></Wave></span>   it is very nice to meet you!</h1>
+                <h2 className={styles.abouth2Class}>I am a passionate aspiring Full Stack Developer skilled in MERN stack, moulded and shaped by Masai school’s intensive and immersive learning.  Look forward to joining a company where I will  be able to contribute towards individual and company growth.</h2>
            
                </div>
             </div>
@@ -135,19 +142,19 @@ const Home = () => {
             
             <Box id="projects" className={classes.projectBox} boxShadow={3}>
                 <h1>My Projects</h1>
-                <Box className={classes.individualProjectBox} boxShadow={3}>
+                <Box data-aos="flip-left" className={classes.individualProjectBox} boxShadow={3}>
                     <Link to="/#" className={styles.externalLinksClass} onClick={()=>window.open("https://github.com/Amol-Talekar/travelocity","_blank")} >travelocity.com</Link>
                     <img className={styles.projectImgClass} alt="travelocity" src="/travelocitylogo.png" />
                     <p className={styles.projectParaClass}>Cloning travelocity.com, a website that caters to all travel needs using HTML, CSS3 and Advanced JavaScript</p>
                 </Box>
                 
-                <Box className={classes.individualProjectBox} boxShadow={3}> 
+                <Box data-aos="flip-right" className={classes.individualProjectBox} boxShadow={3}> 
                 <Link to="/#" className={styles.externalLinksClass} onClick={()=>window.open("https://github.com/Amol-Talekar/unbounce","_blank")} >unbounce.com</Link>
                     <img className={styles.projectImgClass} alt="unbounce" src="/unbouncelogo.png" />
                     <p className={styles.projectParaClass}>Cloning unbounce.com, a website that specializes in creating custom landing pages using HTML, CSS3 and JavaScript</p>
                 </Box>
 
-                <Box className={classes.individualProjectBox} boxShadow={3}>                   
+                <Box data-aos="zoom-in" className={classes.individualProjectBox} boxShadow={3}>                   
                 <Link to="/#" className={styles.externalLinksClass} onClick={()=>window.open("https://github.com/shubham007kumar/Makemytrip","_blank")} >makemytrip.com</Link>
                     <img className={styles.projectImgClass} alt="makemytrip" src="/makemytriplogo1.png" />
                     <p className={styles.projectParaClass}>Cloning makemytrip.com, a website that provides all services while planning a trip using React, Redux, mongoDB, Material UI, HTML, CSS3 and JavaScript</p>
@@ -185,7 +192,7 @@ const Home = () => {
                         variant="contained"
                         color="default"
                         className={classes.resumeButton}
-                        endIcon={<SystemUpdateAltIcon/>}
+                        endIcon={<SystemUpdateAltIcon fontSize="large" />}
                         >
                         Click here to download my Resume
                        </Button>
